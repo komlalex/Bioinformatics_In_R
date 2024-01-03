724c8063d2d1d869b59ea65410b62bf20dbba72d
Kunyelewosi Alexander <kunalexander360@gmail.com>
2024-01-03 08:02:41 +0000

    Initial commit

---
+++ Intro.R
@@ -0,0 +1,15 @@
+# Simple Operations
+
+
+## This is how R does addition
+
+12 + 6
+
+## This is how R does subtraction
+
+12 - 6
+
+## This is how we store data as variables
+## days of the week for this current example
+
+days <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
