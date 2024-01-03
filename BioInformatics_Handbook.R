9773094fed04007c46caba624ba8561450074047
Kunyelewosi Alexander <kunalexander360@gmail.com>
2024-01-03 08:12:53 +0000

    Intro to variables

--- BioInformatics_Handbook.R
+++ BioInformatics_Handbook.R
@@ -13,3 +13,20 @@
 ## days of the week for this current example
 
 days <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+
+# Let's find the 5th entry
+days[5]
+
+# Pull out a range of entries
+days[1:3]
+
+# Pull out the end of the week
+days[5:7]
+
+# Pull out specific days
+days[c(1,2,5,7)]
+
+# Subset our days variable and create weekdays
+weekdays = days[1:5]
+
+print(weekdays)
